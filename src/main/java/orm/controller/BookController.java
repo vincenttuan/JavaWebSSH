4624368700ce219bc7d58aef096e740adace50ad
student <student@192.168.2.19>
2019-05-17 21:33:53 +0800

    JavaWeb

--- src/main/java/orm/controller/BookController.java
+++ src/main/java/orm/controller/BookController.java
@@ -2,12 +2,14 @@ package orm.controller;
 
 import controller.BaseController;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import orm.dao.BookDAOImpl;
 import orm.model.Book;
+import po.MyStock;
 
 @WebServlet("/orm/bookcontroller/*")
 public class BookController extends BaseController {
@@ -27,6 +29,26 @@ public class BookController extends BaseController {
         int id = restRequest.getId();
         Book book = dao.get(id);
         resp.getWriter().print(book);
+    }
+    
+    // 新增資料
+    // http://localhost:8080/SSH/rest/bookcontroller/book/
+    // body : name=xxx&price=xxx
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        try {
+            RestRequest restRequest = new RestRequest(request.getPathInfo());
+            String name = request.getParameter("name");
+            int price = Integer.parseInt(request.getParameter("price"));
+            Book book = new Book(name, price);
+            out.print(dao.create(book));
+        } catch (ServletException e) {
+            e.printStackTrace();
+            out.println(e.toString());
+        }
+
     }
 
 }
